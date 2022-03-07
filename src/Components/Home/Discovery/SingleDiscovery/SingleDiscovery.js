a6777bf218499401f4392c2bd99d4ce3d5b560c2
Zahid <jahidulhaque767@gmail.com>
2022-03-08 03:50:02 +0600

    complete discovery components

--- src/Components/Home/Discovery/SingleDiscovery/SingleDiscovery.js
+++ src/Components/Home/Discovery/SingleDiscovery/SingleDiscovery.js
@@ -3,9 +3,19 @@ import React from "react";
 const SingleDiscovery = (props) => {
   const { img, placeName, properties } = props.discover;
   return (
-    <div>
-      <h2>{placeName}</h2>
-    </div>
+    <>
+      <div className="w-full h-96 ">
+        <img
+          className="h-5/6 w-full rounded-2xl object-cover"
+          src={img}
+          alt=""
+        />
+        <div className="mt-5">
+          <h3 className="text-xl">{placeName}</h3>
+          <span className="text-gray-500">{properties} properties</span>
+        </div>
+      </div>
+    </>
   );
 };
 
