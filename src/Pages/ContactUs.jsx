cf5e6e9feeb1748e563ccc2e9bf8df1b0080af27
Zahid <jahidulhaque767@gmail.com>
2022-03-09 04:05:56 +0600

    added newsletter on contact us components

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Contact from "../Components/ContactUs/Contact";
+import NewsLetter from "../Components/Home/NewsLetter/NewsLetter";
 
 const ContactUs = () => {
   return (
     <div>
       <Contact />
+      <NewsLetter />
     </div>
   );
 };
