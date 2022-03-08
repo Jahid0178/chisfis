b6539bb2f4006e5c8cac35a1dd453cf4cdf70a13
Zahid <jahidulhaque767@gmail.com>
2022-03-09 04:56:37 +0600

    added firebase config and initialization

---
+++ src/Components/Security/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyByhbzJENdMsKKbfEX42We9KOqDJzEZur4",
+  authDomain: "chisfis-adaa4.firebaseapp.com",
+  projectId: "chisfis-adaa4",
+  storageBucket: "chisfis-adaa4.appspot.com",
+  messagingSenderId: "426021463235",
+  appId: "1:426021463235:web:4627ee6601b1ff74dcee76",
+};
+
+export default firebaseConfig;
