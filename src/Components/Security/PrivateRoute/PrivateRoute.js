78161d5a20c293f94683eb54c589af1d58f2ef5e
Zahid <jahidulhaque767@gmail.com>
2022-03-11 05:06:41 +0600

    fixed facebook login to private route

--- src/Components/Security/PrivateRoute/PrivateRoute.js
+++ src/Components/Security/PrivateRoute/PrivateRoute.js
@@ -5,7 +5,7 @@ import useAuth from "../../../Hooks/useAuth";
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth();
   const location = useLocation();
-  return user.email ? (
+  return user.email || user.displayName ? (
     children
   ) : (
     <Navigate to="/sign-up" state={{ from: location }} replace />
